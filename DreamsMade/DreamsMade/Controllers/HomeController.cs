5c0fffdb9cbe86f61936aa55f15403eaeac0c470
Mateus Conde <devmateuscondefeitosa@gmail.com>
2021-12-30 14:58:55 -0300

    Adicionado Controllers e Views

--- DreamsMade/DreamsMade/Controllers/HomeController.cs
+++ DreamsMade/DreamsMade/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using DreamsMade.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace DreamsMade.Controllers
@@ -18,6 +19,71 @@ namespace DreamsMade.Controllers
             return View();
         }
 
+        //---------------------------------------------------------------------------------------------------------------------------
+        public IActionResult MyPage(int id)
+        {
+            Context context = new Context();
+            User? autor = context.Users.Find(id);
+
+            return View(autor);
+        }
+
+        //---------------------------------------------------------------------------------------------------------------------------
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            try
+            {
+                Context context = new Context();
+
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                return RedirectToAction("MyPage", new {id = user.id});
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        //---------------------------------------------------------------------------------------------------------------------------
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            try
+            {
+                //XXXXXXXXXXXXXXXXXXXXXXXXX
+
+                return RedirectToAction("MyPage", new { id = user.id });
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        //---------------------------------------------------------------------------------------------------------------------------
+        
+        [HttpPost]
+        public IActionResult Logout(User user)
+        {
+            //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            return RedirectToAction("Index");
+        }
+
+        //---------------------------------------------------------------------------------------------------------------------------
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
