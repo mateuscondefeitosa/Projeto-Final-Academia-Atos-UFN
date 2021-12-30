eea5b32ba6104d420a26929b487bf4abecd2b3e6
Mateus Conde <devmateuscondefeitosa@gmail.com>
2021-12-30 14:29:57 -0300

    Adicionado funções de criptografia de senha

--- DreamsMade/DreamsMade/Models/Image.cs
+++ DreamsMade/DreamsMade/Models/Image.cs
@@ -1,6 +1,26 @@
-﻿namespace DreamsMade.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DreamsMade.Models
 {
     public class Image
     {
+        [Key]
+        public int id { get; set; }
+
+
+        [StringLength(20)]
+        public string title { get; set; }
+
+
+        [Required]
+        public string image { get; set; }
+
+
+        [StringLength(200)]
+        public string text { get; set; }
+
+
+        public virtual User user { get; set; }
+
     }
 }
--- DreamsMade/DreamsMade/Models/User.cs
+++ DreamsMade/DreamsMade/Models/User.cs
@@ -1,6 +1,24 @@
-﻿namespace DreamsMade.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DreamsMade.Models
 {
     public class User
     {
+        [Key]
+        public int id { get; set; }
+
+
+        [Required]
+        [Display(Name = "User name")]
+        [StringLength(20)]
+        public string name { get; set; }
+
+
+        [Required]
+        [StringLength(20)]
+        public string password { get; set; }
+
+
+        public virtual ICollection<Image> images { get; set; }
     }
 }
