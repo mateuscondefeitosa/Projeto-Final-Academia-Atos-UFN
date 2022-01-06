91534bfca80473125098475d595969f0cfb6ec31
Mateus Conde <devmateuscondefeitosa@gmail.com>
2022-01-06 13:04:24 -0300

    Login Funcionando

--- AulaRestAPI/AulaRestAPI/Models/Pessoa.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AulaRestAPI.Models
-{
-    public class Pessoa
-    {
-        public int id { get; set; }
-        public string nome { get; set; }
-    }
-}
--- AulaRestAPI_TOKENS/AulaRestAPI/Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AulaRestAPI.Models
-{
-    public class Usuario
-    {
-        public string Username { get; set; }
-
-        public string Password { get; set; }
-    }
-}
